e6d19942f8a06ba6147d1d633df7cd238e98acc1
Khristina-code <visockahristina24@gmail.com>
2024-01-24 19:35:44 +0200

    Convert the date format the other way around

--- script1.js
+++ script1.js
@@ -1,18 +1,5 @@
-function addZero(num) {
-	if (num >= 0 && num <= 9) {
-		return '0' + num;
-	} else {
-		return num;
-	}
-}
+let str = '2030-10-12';
+let res = str.split('-').reverse().join('-');
 
-let date = new Date()
+console.log(res)
 
-console.log(
-	addZero(date.getHours()) + ':' +
-	addZero(date.getMinutes()) + ':' +
-	addZero(date.getSeconds()) + ' ' +
-	addZero(date.getDate()) + '.' +
-	addZero(date.getMonth() + 1) + '.' +
-	addZero(date.getFullYear())
-)
