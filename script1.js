8307d3895d9e249bcb9982c2f1c11b948a0dd7f1
Khristina-code <visockahristina24@gmail.com>
2024-01-24 21:47:02 +0200

    Timestamp corresponding to January 1, 2025.

--- script1.js
+++ script1.js
@@ -1,9 +1,5 @@
-let date = new Date(2005, 12, 14)
-let day = date.getDay()
-let days = [
-	'неділя', 'понеділок', 'вівторок', 'середа', 'четвер', 'пятниця', 'субота'
-]
-console.log(days[day])
+let date = new Date(2025, 0, 1);
+console.log(date.getTime());
 
 
 
