dc874319d44430063345492f02ec77c6b414182a
Khristina-code <visockahristina24@gmail.com>
2024-01-24 22:02:06 +0200

    The number of days that have passed between 1 March 1988 and 10 January 2000.

--- script1.js
+++ script1.js
@@ -1,5 +1,8 @@
-let date = new Date(2025, 0, 1);
-console.log(date.getTime());
+let date1  = new Date(1988, 2, 1);
+let date2 = new Date(2000, 0, 10);
+
+let res = date2.getTime() - date1.getTime();
+console.log(res / (1000 * 60 * 60 * 24));
 
 
 
