d8ebb07eb3d4aec3b7cad9d0514e93e8a62add2d
Khristina-code <visockahristina24@gmail.com>
2024-01-24 20:14:01 +0200

    the name of the current month

--- script1.js
+++ script1.js
@@ -1,5 +1,10 @@
 let date = new Date()
-console.log(6 - date.getDay())
+let month = date.getMonth()
+let months = [
+	'січень', 'лютий', 'березень', 'квітень', 'травень', 'червень',
+	'липень', 'серпень', 'вересень', 'жовтень', 'листопад', 'грудень'
+]
+console.log(months[month])
 
 
 
