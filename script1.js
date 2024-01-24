eb02c92f5e812e81da1e5e3a259480663d3eb870
Khristina-code <visockahristina24@gmail.com>
2024-01-24 19:16:32 +0200

    The current time and date, with a function that adds a zero as needed

--- script1.js
+++ script1.js
@@ -1,2 +1,18 @@
-let year = new Date()
-console.log(year.getFullYear())
+function addZero(num) {
+	if (num >= 0 && num <= 9) {
+		return '0' + num;
+	} else {
+		return num;
+	}
+}
+
+let date = new Date()
+
+console.log(
+	addZero(date.getHours()) + ':' +
+	addZero(date.getMinutes()) + ':' +
+	addZero(date.getSeconds()) + ' ' +
+	addZero(date.getDate()) + '.' +
+	addZero(date.getMonth() + 1) + '.' +
+	addZero(date.getFullYear())
+)
