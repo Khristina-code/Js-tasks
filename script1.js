6cc5390a357dcf4761e338537f16ef9c2215314e
Khristina-code <visockahristina24@gmail.com>
2024-01-26 00:43:56 +0200

    Difference in month

--- script1.js
+++ script1.js
@@ -2,7 +2,7 @@ let date1  = new Date(2010, 1, 15);
 let date2 = new Date(2000, 8, 1);
 
 let res = (date1 - date2);
-console.log(res / (1000 * 60 * 60 * 24));
+console.log(res / (1000 * 60 * 60 * 24 * 30));
 
 
 
