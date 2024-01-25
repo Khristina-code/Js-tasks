1109942a59d1e6a1f1351b8fc99b79290064cb8a
Khristina-code <visockahristina24@gmail.com>
2024-01-26 00:00:10 +0200

    The number of months that have passed between 12/14/2005 and the current time

--- script1.js
+++ script1.js
@@ -1,8 +1,8 @@
-let date1  = new Date(1988, 2, 1);
-let date2 = new Date(2000, 0, 10);
+let date1  = new Date(2005, 11, 14);
+let date2 = new Date();
 
-let res = date2.getTime() - date1.getTime();
-console.log(res / (1000 * 60 * 60 * 24));
+let res = (date2.getFullYear() - date1.getFullYear()) * 12 + (date2.getMonth() - date1.getMonth());
+console.log(res);
 
 
 
