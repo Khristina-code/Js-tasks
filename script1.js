e977eeb8ff9d5f636a021a07f731decb9a244f38
Khristina-code <visockahristina24@gmail.com>
2024-01-29 19:43:52 +0200

    Determine how many hours are left until the end of the day.

--- script1.js
+++ script1.js
@@ -1,8 +1,8 @@
-let date = new Date();
-let date1 = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-let date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
+let now  = new Date();
+let date1 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24);
+let date2 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds())
 
-let time = date2.getTime() - date1.getTime()
+let time = date1.getTime() - date2.getTime()
 let hours = time / (1000 * 60 * 60)
 
 console.log(hours)
