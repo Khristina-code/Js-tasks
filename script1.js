cc3a10239a99860d458d0605f65075de0ae4a8f9
Khristina-code <visockahristina24@gmail.com>
2024-01-29 18:03:14 +0200

    Determine how many days there are between January 1 and September 10 of the current year.

--- script1.js
+++ script1.js
@@ -1,7 +1,10 @@
-let now  = new Date();
-let date = new Date(now.getFullYear() - 1, 0, 29);
-console.log(date.getDay())
+let date1 = new Date(2024, 0, 1);
+let date2 = new Date(2024, 8, 10);
 
+let time = date2.getTime() - date1.getTime();
+let days = time / (1000 * 60 * 60 * 24)
+
+console.log(days)
 
 
 
