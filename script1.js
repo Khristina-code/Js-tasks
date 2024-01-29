0b8562ac78626be30bc2882af14d96d7391671b2
Khristina-code <visockahristina24@gmail.com>
2024-01-29 18:39:11 +0200

    Determine how many hours have passed between yesterday's noon and the current time.

--- script1.js
+++ script1.js
@@ -1,9 +1,8 @@
-let date1 = new Date(2024, 0, 20);
-let date2 = new Date(2024, 1, 10);
+let date  = new Date();
+let date1 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds());
+let date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1, 12);
 
-let time = date2.getTime() - date1.getTime()
-let days = time / (1000 * 60 * 60 * 24)
+let time = date1.getTime() - date2.getTime()
+let hours = time / (1000 * 60 * 60)
 
-console.log(days) 
-
-
+console.log(hours)
