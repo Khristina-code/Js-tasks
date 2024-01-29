8159feeca8af258c5a5bdbe51cd349506df490e8
Khristina-code <visockahristina24@gmail.com>
2024-01-29 16:58:15 +0200

    A function that checks the correctness of the date

--- script1.js
+++ script1.js
@@ -1,13 +1,17 @@
-function isLeap(year) {
-  if (new Date(year, 1, 29).getDate() === 29) {
-    return true
-  } else {
-    return false
-  }
+function checkDate(year, month, day) {
+  let date = new Date(year, month, day)
+  if (date.getFullYear() == year && date.getMonth() == month && date.getDate() == day) {
+	return true
+} else {
+	return false
+}
 }
 
 let year = 2024
-console.log(isLeap(year))
+let month = 12
+let day = 12
+
+console.log(checkDate(year, month, day))
 
 
 
