26090f92d917377622379c02ba343cf07a228cef
Khristina-code <visockahristina24@gmail.com>
2024-01-29 18:11:12 +0200

    Determine how many days there are between the 20th of the current month and the 10th of the next month.

--- script1.js
+++ script1.js
@@ -1,10 +1,9 @@
-let date1 = new Date(2024, 0, 1);
-let date2 = new Date(2024, 8, 10);
+let date1 = new Date(2024, 0, 20);
+let date2 = new Date(2024, 1, 10);
 
-let time = date2.getTime() - date1.getTime();
+let time = date2.getTime() - date1.getTime()
 let days = time / (1000 * 60 * 60 * 24)
 
-console.log(days)
-
+console.log(days) 
 
 
