cc9a9d87d4474831b61501b05bc9b8a63b0949b9
Khristina-code <visockahristina24@gmail.com>
2024-01-29 21:49:11 +0200

    How many times has January 1 fallen on a weekend from 2000 to the current year?

--- script1.js
+++ script1.js
@@ -1,8 +1,15 @@
-let now  = new Date();
-let date1 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24);
-let date2 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds())
+function weekendDays() {
+  let count = 0
+  let date1 = 2000
+  let date2 = new Date().getFullYear();
 
-let time = date1.getTime() - date2.getTime()
-let hours = time / (1000 * 60 * 60)
+  for (let year = date1; year <= date2; year++) {
+    let day = new Date(year, 0, 1).getDay()
+    if (day === 0 || day === 1) {
+      count++
+    }
+  }
+  return count
+}
 
-console.log(hours)
+console.log(weekendDays())
