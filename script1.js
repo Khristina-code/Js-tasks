f276d2b7444e5064274545b7dc65d2595136cc2c
Khristina-code <visockahristina24@gmail.com>
2024-01-31 01:45:51 +0200

    Determine how many days are left until the new year.

--- script1.js
+++ script1.js
@@ -1,6 +1,8 @@
 let date = new Date()
-let date1 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 24)
-let date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds())
+let date1 = new Date(2025, 0, 1)
+let date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate())
 
-let seconds = date1.getTime() - date2.getTime()
-console.log(seconds)
+let time = date1.getTime() - date2.getTime()
+let days = time / (1000 * 60 * 60 * 24)
+
+console.log(days)
