618953c8a0190341caf23ef72c42b234f7b12112
Khristina-code <visockahristina24@gmail.com>
2024-01-31 01:38:27 +0200

    Determine how many seconds are left until the end of the day.

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 let date = new Date()
-let date1 = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+let date1 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 24)
 let date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds())
 
-let seconds = date2.getTime() - date1git.getTime()
+let seconds = date1.getTime() - date2.getTime()
 console.log(seconds)
