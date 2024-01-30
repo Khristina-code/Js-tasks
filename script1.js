74f593341c7bd64e74f4d7b7f36d21aa01043b7b
Khristina-code <visockahristina24@gmail.com>
2024-01-31 01:21:12 +0200

    Determine how many seconds have passed from the beginning of the day to the present time

--- script1.js
+++ script1.js
@@ -2,5 +2,5 @@ let date = new Date()
 let date1 = new Date(date.getFullYear(), date.getMonth(), date.getDate())
 let date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds())
 
-let seconds = date1.getTime() - date2.getTime()
+let seconds = date2.getTime() - date1git.getTime()
 console.log(seconds)
