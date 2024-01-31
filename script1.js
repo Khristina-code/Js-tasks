b45533b04fa65b40184674d271f2ab9067c6fd87
Khristina-code <visockahristina24@gmail.com>
2024-01-31 18:11:13 +0200

    Use two dates to determine whether it was noon or not

--- script1.js
+++ script1.js
@@ -1,9 +1,9 @@
 let date = new Date()
 let date1 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds())
-let date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 12)
+let date2 = new Date(date.getFullYear(), date.getMonth(), 15)
 
 if (date1 > date2) {
-  console.log('Вже був опівдень')
+  console.log('Минула половина місяця')
 } else if (date < date2) {
-  console.log('Ще не був опівдень')
+  console.log('Ще не минула половина місяця')
 }
