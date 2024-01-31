275ceed46ec2f261a6f8ddce6fc0e4dfccc0cc40
Khristina-code <visockahristina24@gmail.com>
2024-01-31 15:27:36 +0200

    Determine what year it was 3 months ago.

--- script1.js
+++ script1.js
@@ -1,15 +1,4 @@
-function countFriday13() {
-  let date1 = new Date()
-  let year = date1.getFullYear()
-  let count = 0
+let date = new Date()
+let date1 = new Date(date.getFullYear(), date.getMonth() - 3, date.getDate())
 
-  for (let month = 0; month < 12; month++) {
-    let date2 = new Date(year, month, 13)
-    if (date2.getDay() === 5) {
-      count++
-    }
-  }
-  return count
-}
-
-console.log(countFriday13())
+console.log(date1.getFullYear())
