b4d747a03a7048988495a1a5dca15e5d17c5f702
Khristina-code <visockahristina24@gmail.com>
2024-02-06 23:04:38 +0200

    After hovering over the button, display one message, and after exiting it - another.

--- script1.js
+++ script1.js
@@ -1,4 +1,11 @@
-let button = document.querySelector('#button');
-button.addEventListener('mouseout', function () {
+let button = document.querySelector('#button')
+button.addEventListener('mouseover', func1)
+button.addEventListener('mouseout', func2)
+
+function func1() {
   console.log('hello')
-});
+}
+
+function func2() {
+  console.log('bye')
+}
