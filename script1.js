aa6eb1bf8a678114be53954bc00d05435bb2a286
Khristina-code <visockahristina24@gmail.com>
2024-02-06 22:52:36 +0200

    Double-click the button to display a message.

--- script1.js
+++ script1.js
@@ -1,17 +1,4 @@
-let paragraph = document.querySelector('#elem');
-paragraph.addEventListener('click', func1);
-paragraph.addEventListener('click', func2);
-paragraph.addEventListener('click', func3);
-
-
-function func1() {
-	console.log('1');
-}
-
-function func2() {
-	console.log('2');
-}
-
-function func3() {
-	console.log('3');
-}
+let button = document.querySelector('#button');
+button.addEventListener('dblclick', function () {
+  console.log('hello')
+});
