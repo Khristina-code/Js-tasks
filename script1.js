ca9391db80b7b1a79cef4d95ada9323bab60ea51
Khristina-code <visockahristina24@gmail.com>
2024-02-06 22:43:23 +0200

    Bind functions to a paragraph.

--- script1.js
+++ script1.js
@@ -1,14 +1,17 @@
-let paragraph1 = document.querySelector('#elem1')
-let paragraph2 = document.querySelector('#elem2')
-let paragraph3 = document.querySelector('#elem3')
-let paragraph4 = document.querySelector('#elem4')
-let paragraph5 = document.querySelector('#elem5')
-paragraph1.addEventListener('click', func)
-paragraph2.addEventListener('click', func)
-paragraph3.addEventListener('click', func)
-paragraph4.addEventListener('click', func)
-paragraph5.addEventListener('click', func)
+let paragraph = document.querySelector('#elem');
+paragraph.addEventListener('click', func1);
+paragraph.addEventListener('click', func2);
+paragraph.addEventListener('click', func3);
 
-function func() {
-	console.log('message');
+
+function func1() {
+	console.log('1');
+}
+
+function func2() {
+	console.log('2');
+}
+
+function func3() {
+	console.log('3');
 }
