07bbf0aa5a4abb914e90c41e03bf3379e7648aa1
Khristina-code <visockahristina24@gmail.com>
2024-02-09 00:25:20 +0200

    Two paragraphs containing some numbers in their text and a button are given. After pressing the button, display the sum of the stored numbers on the screen.

--- script1.js
+++ script1.js
@@ -1,9 +1,9 @@
 let button = document.querySelector('#button')
 button.addEventListener('click', func1)
-let elem = document.querySelector('#elem')
+let elem1 = document.querySelector('#elem1')
+let elem2 = document.querySelector('#elem2')
 
 
 function func1() {
-  elem.textContent = 'New paragraph'
-  console.log(elem.textContent)
+  console.log(Number(elem1.textContent) + Number(elem2.textContent))
 }
