8e8919293a156a8bba4f635250f19b66f7b5ceeb
Khristina-code <visockahristina24@gmail.com>
2024-03-04 22:10:56 +0200

    After pressing the button, display the content of the attribute on the screen input type, but not the button

--- script1.js
+++ script1.js
@@ -1,9 +1,8 @@
 let button = document.querySelector('#button')
 button.addEventListener('click', func1)
-let elem1 = document.querySelector('#elem1')
+let elem = document.querySelector('#elem')
 
 
 function func1() {
-  console.log(elem1.innerHTML)
-  elem1.innerHTML = "<b>Hello Christy</b>"
+  console.log(elem.type)
 }
