05a3d7274798959ba0cef04fc6635124024210c7
Khristina-code <visockahristina24@gmail.com>
2024-03-04 20:47:09 +0200

    This is a paragraph with a number and a button. After pressing the button, add one to the value of the paragraph and write the received number back.

--- script1.js
+++ script1.js
@@ -1,9 +1,10 @@
 let button = document.querySelector('#button')
 button.addEventListener('click', func1)
 let elem1 = document.querySelector('#elem1')
-let elem2 = document.querySelector('#elem2')
 
 
 function func1() {
-  console.log(Number(elem1.textContent) + Number(elem2.textContent))
+  let sum = Number(elem1.textContent) + 1
+  elem1.textContent = sum
+  console.log(sum)
 }
