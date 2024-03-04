84642b425877c402452d9cab6b5840d3ca8ab0eb
Khristina-code <visockahristina24@gmail.com>
2024-03-04 21:34:50 +0200

    This is a paragraph of text and a button. By pressing the button, write an exclamation mark at the end of the text of the paragraph.

--- script1.js
+++ script1.js
@@ -4,7 +4,5 @@ let elem1 = document.querySelector('#elem1')
 
 
 function func1() {
-  let sum = Number(elem1.textContent) + 1
-  elem1.textContent = sum
-  console.log(sum)
+  elem1.textContent += "!"
 }
