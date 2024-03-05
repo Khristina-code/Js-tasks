8ca91bd89ba60b8112f99fc71ff0022b2fb38698
Khristina-code <visockahristina24@gmail.com>
2024-03-05 21:45:22 +0200

    After clicking the attribute button width write down the value 300.

--- script1.js
+++ script1.js
@@ -1,9 +1,8 @@
 let button = document.querySelector('#button')
 button.addEventListener('click', func1)
 let image = document.querySelector('#image')
-let paragraph = document.querySelector('#paragraph')
 
 
 function func1() {
-  paragraph.textContent = image.src
+  image.width = 300
 }
