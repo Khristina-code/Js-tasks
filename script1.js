1c5f6a111fc8a72bd82b6903ba722d2096896d6b
Khristina-code <visockahristina24@gmail.com>
2024-03-05 20:16:10 +0200

    After clicking the button, add the content of its attribute to the end of the link text href in round brackets.

--- script1.js
+++ script1.js
@@ -1,9 +1,8 @@
 let button = document.querySelector('#button')
 button.addEventListener('click', func1)
 let link = document.querySelector('#link')
-let paragraph = document.querySelector('#paragraph')
 
 
 function func1() {
-  paragraph.textContent = link.href
+  link.textContent = link.textContent + ' ' + '(' + link.href + ')'
 }
