98e860de48126531e383a1780fe20255423babe4
Khristina-code <visockahristina24@gmail.com>
2024-03-06 19:18:41 +0200

    Data of 5 inputs, a paragraph and a button. Numbers are entered in the input. After pressing the button, write down the arithmetic mean of the entered numbers in a paragraph.

--- script1.js
+++ script1.js
@@ -2,9 +2,13 @@ let button = document.querySelector('#button')
 button.addEventListener('click', func1)
 let elem1 = document.querySelector('#elem1')
 let elem2 = document.querySelector('#elem2')
+let elem3 = document.querySelector('#elem3')
+let elem4 = document.querySelector('#elem4')
+let elem5 = document.querySelector('#elem5')
+let paragraph = document.querySelector('#paragraph')
 
 function func1() {
-  let firstValue = elem1.value
-  elem1.value = elem2.value;
-  elem2.value = firstValue
+  let sum = Number(elem1.value) + Number(elem2.value) + Number(elem3.value) + Number(elem4.value) + Number(elem5.value)
+  let res = sum / 5
+  paragraph.textContent = res
 }
