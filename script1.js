c9a40fbfe519ec9ca62a0e7b2c5f349223f08762
Khristina-code <visockahristina24@gmail.com>
2024-03-07 01:17:35 +0200

    After clicking on the button, write in the attribute class some class of our virgin.

--- script1.js
+++ script1.js
@@ -1,7 +1,10 @@
 let elem = document.querySelector('#elem')
 let button = document.querySelector('#button')
-button.addEventListener('click', function () {
+button.addEventListener('click', func1)
+
+function func1() {
+  elem.className = 'example-class'
   console.log(elem.className)
-})
+}
 
 	
