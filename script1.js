fb71a142aa7a9197fd131c42e677e226539af15e
Khristina-code <visockahristina24@gmail.com>
2024-03-07 00:14:59 +0200

    This input. Let a number be entered into it. After losing focus, display the square of this number on the screen.

--- script1.js
+++ script1.js
@@ -1,14 +1,4 @@
-let button = document.querySelector('#button')
-button.addEventListener('click', func1)
-let elem1 = document.querySelector('#elem1')
-let elem2 = document.querySelector('#elem2')
-let elem3 = document.querySelector('#elem3')
-let elem4 = document.querySelector('#elem4')
-let elem5 = document.querySelector('#elem5')
-let paragraph = document.querySelector('#paragraph')
-
-function func1() {
-  let sum = Number(elem1.value) + Number(elem2.value) + Number(elem3.value) + Number(elem4.value) + Number(elem5.value)
-  let res = sum / 5
-  paragraph.textContent = res
-}
+let elem = document.querySelector('#elem')
+elem.addEventListener('blur', function() {
+	elem.value = elem.value ** 2
+})
