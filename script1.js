07c63d7e2172d354c92b574d5b949d2ff2242e17
Khristina-code <visockahristina24@gmail.com>
2024-03-06 17:48:53 +0200

    After clicking on the first button, write to the attribute src the path to the first picture, and after clicking on the second - the path to the second picture.

--- script1.js
+++ script1.js
@@ -1,8 +1,16 @@
-let button = document.querySelector('#button')
-button.addEventListener('click', func1)
-let image = document.querySelector('#image')
-
+let button1 = document.querySelector('#button1')
+button1.addEventListener('click', func1)
+let button2 = document.querySelector('#button2')
+button2.addEventListener('click', func2)
+let image1 = document.querySelector('#image1')
+let image2 = document.querySelector('#image2')
+let src1 = "https://img.freepik.com/free-photo/fresh-yellow-daisy-single-flower-close-up-beauty-generated-by-ai_188544-15543.jpg?size=626&ext=jpg&ga=GA1.1.1292351815.1709596800&semt=ais"
+let src2 = "https://i.pinimg.com/236x/24/15/21/24152197af38deb718eb730992d441d0.jpg"
 
 function func1() {
- image.width *= 2
+ image1.src = src1
+}
+
+function func2() {
+ image2.src = src2
 }
