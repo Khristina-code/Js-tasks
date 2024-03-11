b87c80fc9fc9e0d4f19b1c12c7470b0b30253410
Khristina-code <visockahristina24@gmail.com>
2024-03-11 20:57:37 +0200

    instead of entering a variable image has chain to be used.

--- script1.js
+++ script1.js
@@ -1,10 +1,2 @@
-let elem = document.querySelector('#elem')
-let button = document.querySelector('#button')
-button.addEventListener('click', func1)
-
-function func1() {
-  elem.className.split(" ")
-  console.log(elem.className)
-}
-
+console.log( document.querySelector('#image').src )
 	
