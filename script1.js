e24cd3a9b529ab2003e7a71336d7d2a88469e6fb
Khristina-code <visockahristina24@gmail.com>
2024-03-11 20:44:24 +0200

    Given a div with several CSS classes written through an omission, a button is also given. After clicking on the button, you will receive an array of CSS classes of our div

--- script1.js
+++ script1.js
@@ -3,7 +3,7 @@ let button = document.querySelector('#button')
 button.addEventListener('click', func1)
 
 function func1() {
-  elem.className = 'example-class'
+  elem.className.split(" ")
   console.log(elem.className)
 }
 
