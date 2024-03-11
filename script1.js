356f60039c7037118634cd5c05177e87878fd596
Khristina-code <visockahristina24@gmail.com>
2024-03-11 21:35:26 +0200

    This input. After receiving focus with this input, write the number 1 to it, and after losing focus - the number 2. Use the object to refer to the input inside the handler function this

--- script1.js
+++ script1.js
@@ -1,4 +1,12 @@
-let elem = document.querySelector('#image')
-elem.width = 300
-elem.height = 500
+let elem = document.querySelector('#elem')
+elem.addEventListener('focus', func1)
+elem.addEventListener('blur', func2)
+
+function func1() {
+console.log(this.value = 1)
+}
+
+function func2() {
+  console.log(this.value = 2)
+}
 	
