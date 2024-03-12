a29dc1b1845f8f6ceebcdbae384c971b099b4f80
Khristina-code <visockahristina24@gmail.com>
2024-03-12 15:36:28 +0200

    Paragraphs with text and a button are given. After clicking on the button, write its serial number at the end of the text of each paragraph.

--- script1.js
+++ script1.js
@@ -1,9 +1,9 @@
-let button = document.querySelector('button')
+let button1 = document.querySelector('#button')
 button.addEventListener('click', func)
 let elems = document.querySelectorAll('.elem')
 
 function func() {
-  for (let elem of elems) {
-    elem.textContent = 'text'
+  for (let i = 0; i < elems.length; i++) {
+    elems[i].textContent += i + 1
   }
 }
