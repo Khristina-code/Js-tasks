a1df218dd0c2d5c4d58a888efff328ce2c41ceb6
Khristina-code <visockahristina24@gmail.com>
2024-03-19 15:33:45 +0200

    Get the years whose sum of digits is 6. Find the sum of the received years.

--- script1.js
+++ script1.js
@@ -1,11 +1,15 @@
-let numbers = document.querySelectorAll('.numbers li')
-for (let number of numbers) {
-  number.addEventListener('click', func)
-}
+let years = document.querySelectorAll('.years li')
+let sum = 0
+for (let i = 0; i < years.length; i++) {
+  let year = years[i].textContent
+  let resultSum = 0
 
-function func() {
-  if (this.textContent < 10) {
-     this.textContent++
+  for (let j = 0; j < year.length; j++) {
+    resultSum += Number(year[j])
   }
-  this.removeEventListener('click', func)
+
+  if (resultSum === 6) {
+    sum += Number(year)
+  }
 }
+console.log(sum)
