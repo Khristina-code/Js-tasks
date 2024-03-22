11efbc989dd01316b887e5cb3d8896ce9f97c51d
Khristina-code <visockahristina24@gmail.com>
2024-03-22 22:07:34 +0200

    When you click on a paragraph, the given text should be added to the end of it

--- script1.js
+++ script1.js
@@ -1,12 +1,7 @@
-let button = document.querySelector('button');
-let elems  = document.querySelectorAll('p');
+let elems = document.querySelectorAll('p');
 
-button.addEventListener('click', function() {
-	let sum = 0;
-	
-	for (let elem of elems) {
-		sum += Number(elem.textContent);
-	}
-	
-	console.log(sum);
-});
+for (let elem of elems) {
+	elem.addEventListener('click', () => {
+   elem.textContent += '!';
+	});
+}
