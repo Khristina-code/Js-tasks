5ae80874fc31220353ff0db617cada12d29fd42f
Khristina-code <visockahristina24@gmail.com>
2024-03-22 23:35:54 +0200

    After clicking on the button, you need to check that the text in each input matches the text of the corresponding element of the array

--- script1.js
+++ script1.js
@@ -1,12 +1,20 @@
 let inputs = document.querySelectorAll('input')
 let button = document.querySelector('#btn')
 
+let texts = [
+	'text1',
+	'text2',
+	'text3',
+];
+
 button.addEventListener('click', function() {
 	for (let input of inputs) {
-		if (input.value === input.dataset.text) {
-			input.classList.add('right')
-		} else {
-			input.classList.add('wrong')
-    }
+		for (let text of texts) {
+			if (inputs.value === text.texts) {
+				input.classList.add('right')
+			} else {
+				input.classList.add('wrong')
+			}
+		}
 	}
 });
