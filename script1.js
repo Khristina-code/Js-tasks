e3f86f340b68ad404a18de779c3d16df83dce4b7
Khristina-code <visockahristina24@gmail.com>
2024-03-22 22:40:39 +0200

    Numbers are entered into the inputs. After clicking on the button, the sum of the numbers from the inputs should be displayed in the console

--- script1.js
+++ script1.js
@@ -1,11 +1,11 @@
 let button = document.querySelector('button');
-let elems  = document.querySelectorAll('p');
+let elems  = document.querySelectorAll('input');
 let sum = 0;
 
 for (let elem of elems) {
-	sum += Number(elem.textContent);
-	
-	button.addEventListener('click', function() {
-		console.log(sum);
-	});
+	sum += Number(elem.value);
 }
+
+button.addEventListener('click', function() {
+	console.log(sum);
+});
