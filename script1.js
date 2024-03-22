849cf642b31ae3711750d2847317f45f6ed49f67
Khristina-code <visockahristina24@gmail.com>
2024-03-23 00:10:50 +0200

    A number is entered in the input. After losing focus, the sum of the digits of this number should be displayed in the console

--- script1.js
+++ script1.js
@@ -1,10 +1,12 @@
-let btn  = document.querySelector('#btn');
-let inp1 = document.querySelector('#inp1');
-let inp2 = document.querySelector('#inp2');
-let inp3 = document.querySelector('#inp3');
+let inp = document.querySelector('#inp');
 
-let sum  = Number(inp1.value) + Number(inp2.value);
-
-btn.addEventListener('click', function() {
-inp3.value = sum;
+inp.addEventListener('blur', function() {
+	let digits = inp.value.split('');
+	let sum = 0;
+	
+	for (let digit of digits) {
+		sum += +digit;
+	}
+	
+	console.log(sum);
 });
