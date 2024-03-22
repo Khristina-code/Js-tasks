e65fe2fed565c306ec7917d0921a1e69798888a9
Khristina-code <visockahristina24@gmail.com>
2024-03-22 21:41:55 +0200

    After clicking on the button, the text of the paragraph should become bold

--- script1.js
+++ script1.js
@@ -1,7 +1,6 @@
-let elems = document.querySelectorAll('p');
+let button = document.querySelector('button');
+let elem   = document.querySelector('p');
 
-for (let elem of elems) {
-	elem.addEventListener('click', function() {
-    this.textContent = Number(this.textContent) + 1;
-	});
-}
+button.addEventListener('click', function() {
+  elem.innerHTML = '<b>text</b>';
+});
