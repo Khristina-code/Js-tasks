f58e2a9aef936ddac53d1b554ed50c28d60bc38c
Khristina-code <visockahristina24@gmail.com>
2024-03-25 20:44:51 +0200

    This input. Enter numbers separated by commas. By loss of focus, find the arithmetic mean of these numbers.

--- script1.js
+++ script1.js
@@ -2,9 +2,10 @@ let input = document.querySelector('#inp');
 
 input.addEventListener('blur', function () {
   let sum = 0 
-  let numbers = input.value.split('')
+  let numbers = input.value.split(',')
   for (let digits of numbers) {
     sum += Number(digits)
   }
-  console.log(sum)
+  let result = sum / numbers.length
+  console.log(result)
 });
