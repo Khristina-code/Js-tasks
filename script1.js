ddce0345b2f27428b9034588e8f155173aa971c2
Khristina-code <visockahristina24@gmail.com>
2024-04-09 15:49:01 +0300

    Make it so that when you click on a view, a purchase price equal to the price multiplied by the quantity is added to the end of its text.

--- script1.js
+++ script1.js
@@ -1,12 +1,4 @@
 let elem = document.querySelector('#elem')
-let min = Number(elem.getAttribute('data-min'))
-let max = Number(elem.getAttribute('data-max'))
-elem.addEventListener('blur', function () {
-    let myValue = elem.value
-    if (myValue.length < min || myValue.length > max) {
-        console.log('кількість введених символів не збігається із заданим')
-    }
-        else {
-            console.log('одинакова кількість')
-        }
-    })
+elem.addEventListener('click', function () {
+    elem.textContent += elem.dataset.productPrice * elem.dataset.productAmount
+})
