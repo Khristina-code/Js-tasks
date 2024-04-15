50208d4da32e250360d044083b7b7b34594b9860
Khristina-code <visockahristina24@gmail.com>
2024-04-15 20:07:47 +0300

    This is a div and a button. Click on the button to add div width, height and border.

--- script1.js
+++ script1.js
@@ -1,3 +1,7 @@
-let elem = document.querySelector('#elem')
-elem.classList.toggle('www')
-
+let div = document.querySelector('#div')
+let button = document.querySelector('#button')
+button.addEventListener('click', function () {
+    div.style.width = '200px'
+    div.style.length = '150px'
+    div.style.border = '5px solid black'
+})
