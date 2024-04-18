739712c2787cd70509e9c2bac9e8ce4054e8953e
Khristina-code <visockahristina24@gmail.com>
2024-04-18 21:47:17 +0300

    This input and two buttons. Let pressing the first button block the input, and pressing the second button - unlock it.

--- script1.js
+++ script1.js
@@ -1,7 +1,11 @@
 let elem = document.querySelector('#elem')
-let button = document.querySelector('#button')
-button.addEventListener('click', function () {
+let button1 = document.querySelector('.button1')
+let button2 = document.querySelector('.button2')
+button1.addEventListener('click', function () {
   elem.disabled = true
+})
+button2.addEventListener('click', function () {
+  elem.disabled = false
 })
 
 
