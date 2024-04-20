e2d91820dcd7e4126c4880bac82e56cb76e9c22a
Khristina-code <visockahristina24@gmail.com>
2024-04-20 14:22:44 +0300

    This checkbox and button. Make it so that every click on the button leads to a change in the state of the checkbox.

--- script1.js
+++ script1.js
@@ -1,14 +1,8 @@
 let elem = document.querySelector('#elem')
 let button = document.querySelector('#button')
-let paragraph = document.querySelector('#paragraph')
 
 button.addEventListener('click', function () {
-  if (elem.checked) {
-    paragraph.textContent = 'hello'
-  } else {
-    paragraph.textContent = 'bye'
-  }
- 
+  elem.checked = !elem.checked
 })
 
 
