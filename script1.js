b300f4c2cf8e8ecf4dc5befd189300aa090cb9cd
Khristina-code <visockahristina24@gmail.com>
2024-04-23 20:21:52 +0300

    This checkbox. After changing the checkbox, display its new state on the screen.

--- script1.js
+++ script1.js
@@ -1,8 +1,7 @@
 let elem = document.querySelector('#elem');
-let paragraph = document.querySelector('#paragraph')
 
 elem.addEventListener('change', function() {
-	paragraph.textContent = elem.value
+	alert(elem.checked)
 });
 
 
