1570b52f1600238728d25c8eed7ff6ea0dc3bfdc
Khristina-code <visockahristina24@gmail.com>
2024-04-29 20:46:22 +0300

    Two inputs are given. Make it so that after entering two characters, the input focus goes to the second input, and after entering two characters into this input, the focus is taken away from it.

--- script1.js
+++ script1.js
@@ -1,9 +1,16 @@
-let elem = document.querySelector('#elem');
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
 
-elem.addEventListener('input', function() {
-  if (elem.value.length >= 5) {
-    console.log('довжина досягла 5')
+elem1.addEventListener('input', function() {
+  if (elem1.value.length === 2) {
+    elem2.focus()
   }
-});
+})
 
+elem2.addEventListener('input', function() {
+  if (elem2.value.length === 2) {
+    elem1.focus()
+    elem2.blur()
+  }
+})
 
