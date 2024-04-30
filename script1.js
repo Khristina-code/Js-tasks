1652dc3c5254e46445aee8425b07efedfb29bd96
Khristina-code <visockahristina24@gmail.com>
2024-04-30 16:39:55 +0300

    Make a drop-down list with the names of the days of the week. As attributes value list items, add the numbers of days of the week from 1 to 7. After changing the list, display a message about the selected holiday or working day on the screen.

--- script1.js
+++ script1.js
@@ -1,12 +1,10 @@
-let select = document.querySelector('#select');
+let select = document.querySelector('#select')
 
 select.addEventListener('change', function () {
-  let year = select.value
-    if (new Date(year, 1, 29).getDate() === 29) {
-      console.log('це високосний рік') 
-    } else {
-        console.log('не високосний рік')
-      }
-    
-})
+  if (this.value <= 5) {
+    console.log('work day')
+  } else {
+    console.log('weekend')
+  }
+});
 
