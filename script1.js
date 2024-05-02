95b1b5c895c7fcb119aff921cd73b9e25d7d3c65
Khristina-code <visockahristina24@gmail.com>
2024-05-02 20:56:25 +0300

    Make a button, pressing which will display a countdown in the console, starting from 100

--- script1.js
+++ script1.js
@@ -3,7 +3,7 @@ let start = document.querySelector('#start')
 start.addEventListener('click', function () {
 	let i = 100
 
-let timerId = setInterval(function () {
+ 	setInterval(function () {
 	console.log(--i)
 }, 1000)
 })
