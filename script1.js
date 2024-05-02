41f591527786ce2f4ddb4b331d8817615e03e595
Khristina-code <visockahristina24@gmail.com>
2024-05-02 19:41:17 +0300

    Write the bind method so that this inside the func function always points to the input with the elem variable

--- script1.js
+++ script1.js
@@ -1,7 +1,10 @@
-let elem = document.querySelector('#elem');
+let elem = document.getElementById('elem');
 
 function func(name, surname) {
 	console.log(this.value + ', ' + name + ' ' + surname);
 }
 
 func.apply(elem, ['John', 'Smit']);
+
+func = func.bind(elem)
+func('Eric', 'Luis')
