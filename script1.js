92ac3dafefd04fd48a964c28327292d30c4a3e78
Khristina-code <visockahristina24@gmail.com>
2024-05-04 10:15:21 +0300

    Let in the input in the attribute value first the number 10 is recorded. Start a timer that will decrease this number by one every second.

--- script1.js
+++ script1.js
@@ -1,16 +1,6 @@
-let start = document.querySelector('#start');
-let stop  = document.querySelector('#stop');
+let elem = document.querySelector('#elem');
 
-let timerId;
-
-start.addEventListener('click', function() {
-		timerId = setInterval(function() {
-		let date = new Date;
-		console.log(date.getMinutes() + ' ' + date.getSeconds());
-	}, 1000);
-});
-
-stop.addEventListener('click', function() {
-	clearInterval(timerId);
-});
+setInterval(function() {
+	elem.value = Number(elem.value) - 1;
+}, 1000);
 
