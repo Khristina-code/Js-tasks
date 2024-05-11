af5ad07d9980d96685d83229cc7d03a99ad6ef45
Khristina-code <visockahristina24@gmail.com>
2024-05-11 13:36:35 +0300

    Click the button to increase the width and height of the div by 50px.

--- script1.js
+++ script1.js
@@ -1,7 +1,15 @@
-let elem = document.querySelector('#elem')
+let elem = document.querySelector('div');
 let button = document.querySelector('#button')
 
 button.addEventListener('click', function () {
-	console.log(parseFloat(elem.style.fontSize))
+	let widthPx = elem.style.width
+	let widthNm = parseInt(widthPx)
+	elem.style.width = (widthNm + 50) + 'px'
+
+	let heightPx = elem.style.height
+	let heightNm = parseInt(heightPx)
+	elem.style.height = (heightNm + 50) + 'px'
 })
+
+
 
