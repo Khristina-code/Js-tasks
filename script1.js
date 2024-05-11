4737b73c1cab33d6f446d111fc11078d9d4f5d84
Khristina-code <visockahristina24@gmail.com>
2024-05-11 12:54:43 +0300

    There is also a button. Click the button to display the unitless miracle font size.

--- script1.js
+++ script1.js
@@ -2,7 +2,6 @@ let elem = document.querySelector('#elem')
 let button = document.querySelector('#button')
 
 button.addEventListener('click', function () {
-	console.log(parseInt(elem.style.width))
-	console.log(parseInt(elem.style.height))
+	console.log(parseFloat(elem.style.fontSize))
 })
 
