81691ae38d3f7d369c31d60a10859ba3279e3dd6
Khristina-code <visockahristina24@gmail.com>
2024-05-11 14:09:11 +0300

    After clicking on the button, display the thickness of the border, its type and color.

--- script1.js
+++ script1.js
@@ -2,13 +2,7 @@ let elem = document.querySelector('div');
 let button = document.querySelector('#button')
 
 button.addEventListener('click', function () {
-	let widthPx = elem.style.width
-	let widthNm = parseInt(widthPx)
-	let heightPx = elem.style.height
-	let heightNm = parseInt(heightPx)
-
-	elem.style.width = (widthNm * 1.1) + 'px'
-	elem.style.height = (heightNm * 1.1) + 'px'
+	console.log(elem.style.border)
 })
 
 
