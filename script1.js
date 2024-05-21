5a8594f5190502db0ec6d906ee11a19ef854c377
Khristina-code <visockahristina24@gmail.com>
2024-05-21 19:44:14 +0300

    Inputs are given. Each input can be entered as a number. Let the input remember the entered number after pressing Enter. Make it so that after losing focus in the input, an array of all previously entered numbers in the input is output to the console.

--- script1.js
+++ script1.js
@@ -2,13 +2,23 @@ let elems = document.querySelectorAll('.elem')
 
 let map = new Map;
 
-let i = 1
 for (let elem of elems) {
-	map.set(elem, i++);
+	map.set(elem, []);
 }
 
 for (let elem of elems) {
-	elem.addEventListener('click', function() {
-		this.value += map.get(this);
+	elem.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      let value = parseFloat(this.value);
+    if (!isNaN(value)) {
+       map.get(this).push(value);
+       this.value = ''; 
+    }
+		}
 	});
+
+            
+  elem.addEventListener('blur', function() {
+		console.log(map.get(this));
+  });
 }
