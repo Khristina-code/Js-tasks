bd3ea9ea356429cd676122927571f1069a92db3f
Khristina-code <visockahristina24@gmail.com>
2024-06-01 15:12:56 +0300

    Get the collection iterator Map . Call it successively until the collection runs out.

--- script1.js
+++ script1.js
@@ -1,19 +1,11 @@
-let obj1 = { a: 1, b: 2, c: 3 }
-let obj2 = {e: 4, f: 5, g: 6}
+let map = new Map()
+map.set('a', 1)
+map.set('b', 2)
+map.set('c', 3)
 
-let sym = Symbol.for('sum')
+let iter = map[Symbol.iterator]();
 
-function func () {
-	let sum = 0
-
-	for (let key in this) {
-		sum += this[key]
-	}
-	return sum
-}
-
-obj1[sym] = func
-obj2[sym] = func
-console.log(obj1[sym]())
-console.log(obj2[sym]())
-
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
