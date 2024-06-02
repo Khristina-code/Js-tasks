52de35aadc9d4a2c55c6ee5c364b40bfc2f0ea06
Khristina-code <visockahristina24@gmail.com>
2024-06-02 13:04:48 +0300

    Make an iterator, each call of which will return the next Fibonacci number.

--- script1.js
+++ script1.js
@@ -1,12 +1,18 @@
-function* func(num) {
-  while (num >= 0) {
-    yield num
-    num -= 2
+function* func() {
+  let prev = 0
+  let curr = 1
+  yield prev;
+  yield curr;
+  while (true) {
+    let next = prev + curr;
+    yield next;
+    prev = curr;
+    curr = next;
   }
 }
 
-let generator = func(8)
+let generator = func()
 
-for (let value of generator) {
-  console.log(value)
+for (let i = 0; i < 10; i++) {
+  console.log(generator.next().value)
 }
